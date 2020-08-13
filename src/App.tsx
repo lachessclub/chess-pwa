66679a05b090983556a2f22c3848ed8df7079520
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-08-14 01:13:45 +0500

    fix: formatting and tests (#9)
    * fix: formatting (call yarn format)
    
    * fix tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
 import React, { FC } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App: FC = () => {
   return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/game">Game</Link>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game">Game</Link>
+            </li>
+          </ul>
+        </nav>
 
-          <Switch>
-            <Route path="/game">
-              Game
-            </Route>
-            <Route path="/">
-              Home
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+        <Switch>
+          <Route path="/game">Game</Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
