a8334ab41c62219bb84e434c6ce5aae0f0c6f352
ildar-icoosoft <ildar-icoosoft@users.noreply.github.com>
2020-08-13 03:35:06 +0000

    deploy: 6079298aa7144611da6ce1df235e54ab471542c5

---
+++ precache-manifest.d33a970ea6f475f24758c19ef961beb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdc228a676de4e06b2b8d8d9200f6e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ab590203d2c963e9ee",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3f150912c9af3a9e4ba",
+    "url": "/static/js/2.d1a9bdbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d1a9bdbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ab590203d2c963e9ee",
+    "url": "/static/js/main.490a308a.chunk.js"
+  },
+  {
+    "revision": "3e2924cacdc6467286cb",
+    "url": "/static/js/runtime-main.d45438de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
