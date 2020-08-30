e115c6f14b66f325256bc82a984def12b9828515
ildar-icoosoft <ildar-icoosoft@users.noreply.github.com>
2020-08-30 18:44:30 +0000

    deploy: 58ea41a2b96efc9900bf54e203d2711ad8384737

---
+++ precache-manifest.5bcf5a7211f18c6eac69adcb543c5766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4214a2c21ff4c04eda20046e40aeaf4e",
+    "url": "/chess-pwa/index.html"
+  },
+  {
+    "revision": "e6105df33f51947de747",
+    "url": "/chess-pwa/static/css/main.179d0da0.chunk.css"
+  },
+  {
+    "revision": "f229e877020c5b9b6357",
+    "url": "/chess-pwa/static/js/2.d6500539.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/chess-pwa/static/js/2.d6500539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6105df33f51947de747",
+    "url": "/chess-pwa/static/js/main.48cee906.chunk.js"
+  },
+  {
+    "revision": "d0774963cc441ee5a4c2",
+    "url": "/chess-pwa/static/js/runtime-main.cbb0f1fa.js"
+  }
+]);
