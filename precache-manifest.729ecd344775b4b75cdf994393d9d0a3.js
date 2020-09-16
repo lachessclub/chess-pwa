57122551f7e045357e855bbf5258d5ec1a27965b
ildar-icoosoft <ildar-icoosoft@users.noreply.github.com>
2020-09-16 15:13:02 +0000

    deploy: f0205b25b79c95311155c0f0616c196ea4c5cd96

--- precache-manifest.729ecd344775b4b75cdf994393d9d0a3.js
+++ precache-manifest.729ecd344775b4b75cdf994393d9d0a3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6f3302a75b5bc0b6fc61949f2efa6fbf",
+    "revision": "72370cca98ee092d5846eb0413f2cc2f",
     "url": "/chess-pwa/index.html"
   },
   {
-    "revision": "350617b9573d19766c47",
+    "revision": "797cd76aa549acc69e3a",
     "url": "/chess-pwa/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "d6aadfd7e20559fe0cee",
+    "revision": "d100d5d2ff6387d9d796",
     "url": "/chess-pwa/static/css/main.1ba81c95.chunk.css"
   },
   {
-    "revision": "350617b9573d19766c47",
-    "url": "/chess-pwa/static/js/2.72610633.chunk.js"
+    "revision": "797cd76aa549acc69e3a",
+    "url": "/chess-pwa/static/js/2.b8b20f36.chunk.js"
   },
   {
     "revision": "4aba7b7146786ddd2b0b5790d0896d2e",
-    "url": "/chess-pwa/static/js/2.72610633.chunk.js.LICENSE.txt"
+    "url": "/chess-pwa/static/js/2.b8b20f36.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d6aadfd7e20559fe0cee",
-    "url": "/chess-pwa/static/js/main.761089bf.chunk.js"
+    "revision": "d100d5d2ff6387d9d796",
+    "url": "/chess-pwa/static/js/main.6e10ca13.chunk.js"
   },
   {
     "revision": "d0774963cc441ee5a4c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chess-pwa/precache-manifest.3748468c2c148a57e6e877d6ef96f7d1.js"
+  "/chess-pwa/precache-manifest.729ecd344775b4b75cdf994393d9d0a3.js"
 );
 
 self.addEventListener('message', (event) => {
