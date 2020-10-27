07c9283b878a2cc20a2bb23a46619939bd5a538d
ildar-icoosoft <ildar-icoosoft@users.noreply.github.com>
2020-10-27 16:23:49 +0000

    deploy: 2df2419a80797dcc684852fcf903bd0b2a9cd7e9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chess-pwa/precache-manifest.655910b2c29d9790a796cee130664668.js"
+  "/chess-pwa/precache-manifest.b47cbceb1ccb79928fc321e4f5090fd4.js"
 );
 
 self.addEventListener('message', (event) => {
