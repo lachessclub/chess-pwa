7609fb4137c5acf5d23c9edc862e0bc84ba18389
ildar-icoosoft <ildar.icoosoft@gmail.com>
2020-10-28 17:03:45 +0500

    feat: CHESSPWA-73 create config from environment variables (#100)

--- src/services/ioClient.ts
+++ src/services/ioClient.ts
@@ -3,6 +3,6 @@ import sailsIOClient from "sails.io.js";
 
 const ioClient = sailsIOClient(socketIOClient);
 
-ioClient.sails.url = "http://localhost:1337";
+ioClient.sails.url = process.env.REACT_APP_BACKEND_HOST;
 
 export default ioClient;
